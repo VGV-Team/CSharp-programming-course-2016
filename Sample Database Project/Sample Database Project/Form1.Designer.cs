65d42b4275d5c7b34911ecf14b5e403e9569952b
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2016-12-06 19:00:05 +0100

    added sample database for in class handouts

---
+++ Sample Database Project/Sample Database Project/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Sample_Database_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bRead = new System.Windows.Forms.Button();
+            this.Write = new System.Windows.Forms.Button();
+            this.bInsert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bRead
+            // 
+            this.bRead.Location = new System.Drawing.Point(197, 226);
+            this.bRead.Name = "bRead";
+            this.bRead.Size = new System.Drawing.Size(75, 23);
+            this.bRead.TabIndex = 0;
+            this.bRead.Text = "Read";
+            this.bRead.UseVisualStyleBackColor = true;
+            this.bRead.Click += new System.EventHandler(this.bRead_Click);
+            // 
+            // Write
+            // 
+            this.Write.Location = new System.Drawing.Point(12, 226);
+            this.Write.Name = "Write";
+            this.Write.Size = new System.Drawing.Size(75, 23);
+            this.Write.TabIndex = 1;
+            this.Write.Text = "bWrite";
+            this.Write.UseVisualStyleBackColor = true;
+            this.Write.Click += new System.EventHandler(this.Write_Click);
+            // 
+            // bInsert
+            // 
+            this.bInsert.Location = new System.Drawing.Point(12, 12);
+            this.bInsert.Name = "bInsert";
+            this.bInsert.Size = new System.Drawing.Size(260, 50);
+            this.bInsert.TabIndex = 2;
+            this.bInsert.Text = "Insert";
+            this.bInsert.UseVisualStyleBackColor = true;
+            this.bInsert.Click += new System.EventHandler(this.bInsert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bInsert);
+            this.Controls.Add(this.Write);
+            this.Controls.Add(this.bRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bRead;
+        private System.Windows.Forms.Button Write;
+        private System.Windows.Forms.Button bInsert;
+    }
+}
+
---
+++ Sample Database Project/Sample Database Project/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample_Database_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Write_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection myConnection = new SqlConnection("Server=tcp:c-sharp-course.database.windows.net,1433;Initial Catalog=C_Sharp_Course_Sample_Database;Persist Security Info=False;User ID=nsk;Password=QWEqwe123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            SqlCommand qc = new SqlCommand("INSERT INTO SampleTable VALUES ('1QWE1',1)", myConnection);
+
+            myConnection.Open();
+
+            qc.ExecuteNonQuery();
+
+            myConnection.Close();
+        }
+
+        private void bRead_Click(object sender, EventArgs e)
+        {
+            SqlConnection myConnection = new SqlConnection("Server=tcp:c-sharp-course.database.windows.net,1433;Initial Catalog=C_Sharp_Course_Sample_Database;Persist Security Info=False;User ID=nsk;Password=QWEqwe123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            myConnection.Open();
+
+            SqlCommand qc = new SqlCommand("SELECT * FROM SampleTable", myConnection);
+            SqlDataReader reader = qc.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetInt32(2));
+                }
+            }
+            else
+            {
+                MessageBox.Show("No rows found.");
+            }
+
+            myConnection.Close();
+        }
+    }
+}
